361ecc2121a42407d93bdeabce159a2beac166a5
Thapus Simon <thapus95@gmail.com>
2021-04-04 21:54:48 +0530

    search box implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Component} from 'react';
+import CardList from '../src/components/card-list/card-list.component';
+import { SearchBox } from '../src/components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super();
+    //The super calls the constructor of the component class 
+    //Then we can have access to something called state
+    this.state={
+      monsters:[],
+      searchField:''
+    }
+    
+  }
+
+  //After react places the component on our page this function gets called
+  //We use fetch api to go to the end point & return a promise
+  //& then we return another promise which is json object & update monsters with the users
+  componentDidMount(){
+      fetch("http://jsonplaceholder.typicode.com/users")
+        .then(response=>response.json())
+        .then(users=>this.setState({ monsters:users}))
+  }
+
+
+  handleChange=(e)=>{
+      this.setState({searchField:e.target.value},()=>{console.log(this.state)});
+
+  }
+
+
+  render(){
+
+      const { monsters,searchField } =this.state;
+      const filteredMonsters=monsters.filter(monster=>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+    
+    return(
+
+      <div className="App">
+
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+          />
+
+        <CardList monsters={filteredMonsters} />
+        
+  
+       
+      </div>
+    )
+      
+  }
 }
-
 export default App;
